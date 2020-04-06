3e4125f1ebe2a859d1ac4b2669f383cff195d239
Dean Wampler <dean@concurrentthought.com>
2020-04-06 15:53:55 -0700

    Refined the implementation

--- util/printing.py
+++ util/printing.py
@@ -1,5 +1,12 @@
 # Convenience utilities for printing
 
-def p(n, duration):
-    print('{:2d}: {:6.3f} seconds'.format(n, duration))
+def pnd(n, duration, prefix=''):
+    """Print an integer and a time duration, with an optional prefix."""
+    prefix2 = prefix if len(prefix) == 0 else prefix+' '
+    print('{:s}n: {:2d}, duration: {:6.3f} seconds'.format(prefix2, n, duration))
+
+def pd(duration, prefix=''):
+    """Print a time duration, with an optional prefix."""
+    prefix2 = prefix if len(prefix) == 0 else prefix+' '
+    print('{:s}duration: {:6.3f} seconds'.format(prefix2, duration))
 
