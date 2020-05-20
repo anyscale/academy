458a40086339c7b7f9f41e6d89c23eef144ade13
Dean Wampler <dean@concurrentthought.com>
2020-05-20 06:21:55 -0700

    Bumped Ray version

--- tools/setup-jupyter.sh
+++ tools/setup-jupyter.sh
@@ -1,32 +1,57 @@
 #!/usr/bin/env bash
 
-# What's expected in an Anyscale cluster (at this time)
 expected_conda_env=tensorflow_p36
 
+help() {
+	cat <<EOF
+Complete setup of the Jupyter Lab environment.
+Usage: $0 [-h|--help] [-e|--env ENV]
+Where:
+	-h | --help        Print this message and exit
+	-e | --env  ENV    Use this Conda environment (default: $expected_conda_env)
+EOF
+}
+
+while [[ $# -gt 0 ]]
+do
+	case $1 in
+		-h|--help)
+			help
+			exit 0
+			;;
+		-e|--env)
+			shift
+			expected_conda_env=$1
+			;;
+		*)
+			echo "ERROR: Unexpected argument $1"
+			help
+			exit 1
+			;;
+	esac
+	shift
+done
+
+# What's expected in an Anyscale cluster (at this time)
+
 conda_env=$(conda info | grep 'active environment' | sed -e 's/^[^:]*: *//g')
-if [[ $conda_env = 'base' ]] || [[ $conda_env = 'None' ]]
+if [[ $conda_env != $expected_conda_env ]]
 then
-	# Only do these steps if the user is not already in a non-base environment
-	# We don't specifically expect $expected_conda_env, because the actual
-	# environment may be different.
-	echo "Activating $expected_conda_env conda environment"
-	source $HOME/anaconda3/etc/profile.d/conda.sh
-	conda activate $expected_conda_env
+	$NOOP source $HOME/anaconda3/etc/profile.d/conda.sh
+	echo "running: conda activate $expected_conda_env"
+	$NOOP conda activate $expected_conda_env
 fi
 
 echo "Node.js required for some Jupyter Lab extensions. Installing if necessary..."
-conda install -y nodejs
+$NOOP conda install -y nodejs
 
-extensions=(
-	@pyviz/jupyterlab_pyviz
-#	@jupyter-widgets/jupyterlab-manager
-#	@bokeh/jupyter_bokeh
-)
-echo "Checking or adding extensions to Jupyter Lab:"
+extensions=( @pyviz/jupyterlab_pyviz )
+
+echo "Adding extensions to Jupyter Lab, as needed:"
 for ext in "${extensions[@]}"
 do
 	echo "extension: $ext"
-	jupyter labextension check --installed "$ext" || jupyter labextension install "$ext"
+	$NOOP jupyter labextension check --installed "$ext" || jupyter labextension install "$ext"
 done
 echo "All extensions:"
-jupyter labextension list
+$NOOP jupyter labextension list
